a14b0505ab65374342a6063a316379752253fd7d
Josué <josuecarlosos2@gmail.com>
2023-03-06 03:44:12 -0300

    intro

---
+++ intro.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//gcc intro.cc -o prog.out
+//compilador gcc
+int main(int argc, char const *argv[])
+{
+    printf("hi");
+    int numero = 9;
+    int numero2 = 9;
+    double a = (double) numero2;
+    double a = abs(-2);
+    double as =  rand() % 100;
+    srand(time(0));
+
+    printf(" dsds %d", numero);
+    printf("qual o numero")
+    scanf("%d", &numero2);
+
+    if (/* condition */)
+    {
+        /* code */
+    }else if (/* condition */)
+    {
+        /* code */
+    }else
+    {
+        /* code */
+    }
+
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+        break;
+        continue;
+    }
+
+    while (/* condition */)
+    {
+        /* code */
+    }
+
+    do
+    {
+        /* code */
+    } while (/* condition */);
+    
+    
+    
+    
+    
+    
+    /* code */
+    return 0;
+}
