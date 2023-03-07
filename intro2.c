01009b92e086855b972ce2639e8b3a1e17d128bc
Josué <josuecarlosos2@gmail.com>
2023-03-06 23:03:59 -0300

    intro array ponteiro

---
+++ intro2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    int array[10];
+    array[0] = 1;
+    int tamanho = strlen("word");
+
+    //NUMERO BINARIO
+    
+    return 0;
+}
